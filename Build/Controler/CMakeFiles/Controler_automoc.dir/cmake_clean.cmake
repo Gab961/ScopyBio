71be605be0d9f5b5aa7397a2b9511456a70db266
Prozengan <prozengan@gmail.com>
2019-10-23 15:27:01 +0200

    Build files removal

--- Build/Controler/CMakeFiles/Controler_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Controler_automoc.cpp"
-  "CMakeFiles/Controler_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Controler_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
