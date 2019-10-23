34bed4b2ba04642d88267f45d2ebe58d25dcd487
Satou <thomas.saout.pro@gmail.com>
2019-10-23 09:57:20 +0200

    First test gitKraken
    modify pile model

--- Model/pile_model.h
+++ Model/pile_model.h
@@ -1,9 +1,14 @@
 #ifndef PILE_MODEL_H
 #define PILE_MODEL_H
 
+#include <vector>
+
+#include "image_model.h"
 
 class pile_model
 {
+private:
+    std:vector<image_model> images;
 public:
     pile_model();
 };
