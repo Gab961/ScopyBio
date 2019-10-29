94b2e9c847398ffa967f6ed7fd9c6d9af9e60a22
Gabriel <devilheart_15@hotmail.com>
2019-10-29 13:55:44 +0100

    Constructor of memento

--- Model/annotation_user_memento.h
+++ Model/annotation_user_memento.h
@@ -1,5 +1,22 @@
 #ifndef ANNOTATION_USER_MEMENTO_H
 #define ANNOTATION_USER_MEMENTO_H
 
+class annotation_user_memento
+{
+
+private:
+  friend class image_model;
+  std::string path;
+
+public:
+
+    annotation_user_memento(std::string _path)
+    {
+        _path = path;
+    }
+
+
+};
+
 #endif // ANNOTATION_USER_MEMENTO_H
 
