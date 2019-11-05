431d7d7b9fb91b7d40fcbb4d63f626456a60e261
Erinyth <annalyhse@gmail.com>
2019-11-05 17:19:00 +0100

    Correction zoom

--- View/zoom_view.cpp
+++ View/zoom_view.cpp
@@ -23,55 +23,32 @@ void Zoom_View::setNewPicture(int zoneWidth, int zoneHeight)
     m_image->setFixedWidth(zoneWidth);
     m_image->setFixedHeight(zoneHeight);
 
-//     Largeur du widget <= hauteur
-//     Sert à créer une image qui va prendre un maximum de place possible
-//     sans empiéter sur les autres widgets
+    //     Largeur du widget <= hauteur
+    //     Sert à créer une image qui va prendre un maximum de place possible
+    //     sans empiéter sur les autres widgets
     if (m_image->size().width() >= m_image->size().height()) {
-        std::cout << "LARGE" << std::endl;
-        std::cout << "Largeur widget : " << size().width()
-                  << " - Hauteur widget : " << size().height() << std::endl;
         if (m_image->size().width() >= size().width()) {
-            std::cout << "--- Image plus large ---" << std::endl;
             ratio = (float)m_image->size().width() / (float)size().width();
+            m_image->setFixedWidth(size().width());
+            m_image->setFixedHeight(static_cast<int>(m_image->size().height()/ratio));
         }
         else {
-            std::cout << "--- Widget plus large ---" << std::endl;
             ratio = (float)size().width() / (float)m_image->size().width();
+            m_image->setFixedWidth(size().width());
+            m_image->setFixedHeight(static_cast<int>(m_image->size().height()*ratio));
         }
-
-        std::cout << "Ratio : " << ratio << std::endl;
-        std::cout << "Largeur image avant modification : " << m_image->size().width()
-                  << " - Hauteur image avant modification : " << m_image->size().height() << std::endl;
-
-        m_image->setFixedWidth(size().width());
-        m_image->setFixedHeight(static_cast<int>(m_image->size().height()*ratio));
-
-        std::cout << "Largeur image après modification : " << m_image->size().width()
-                  << " - Hauteur image après modification : " << m_image->size().height() << std::endl;
     }
-    else
-    {
-        std::cout << "LONG" << std::endl;
-        std::cout << "Largeur widget : " << size().width()
-                  << " - Hauteur widget : " << size().height() << std::endl;
+    else {
         if (m_image->size().height() >= size().height()) {
-            std::cout << "--- Image plus longue ---" << std::endl;
             ratio = (float)m_image->size().height() / (float)size().height();
+            m_image->setFixedWidth(static_cast<int>(m_image->size().width()/ratio));
+            m_image->setFixedHeight(size().height());
         }
         else {
-            std::cout << "--- Widget plus long ---" << std::endl;
             ratio = (float)size().height() / (float)m_image->size().height();
+            m_image->setFixedWidth(static_cast<int>(m_image->size().width()*ratio));
+            m_image->setFixedHeight(size().height());
         }
-
-        std::cout << "Ratio : " << ratio << std::endl;
-        std::cout << "Largeur image avant modification : " << m_image->size().width()
-                  << " - Hauteur image avant modification : " << m_image->size().height() << std::endl;
-
-        m_image->setFixedWidth(static_cast<int>(m_image->size().width()*ratio));
-        m_image->setFixedHeight(size().height());
-
-        std::cout << "Largeur image après modification : " << m_image->size().width()
-                  << " - Hauteur image après modification : " << m_image->size().height() << std::endl;
     }
 
     m_layout->addWidget(m_image);
