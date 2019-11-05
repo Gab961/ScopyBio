57ca9998b3cec8a0fccaf185ce269a2a92701217
Prozengan <prozengan@gmail.com>
2019-11-05 17:22:10 +0100

    Correction mineure selection

--- Model/dessin_model.cpp
+++ Model/dessin_model.cpp
@@ -14,20 +14,6 @@ void dessin_model::dessinerRectangle(QPoint pos1, QPoint pos2, int labelWidth, i
     int x2 = pos2.x() * currentPicture.width() / labelWidth;
     int y2 = pos2.y() * currentPicture.height() / labelHeight;
 
-    std::cout << "Image: " << currentPicture.width() << "x" << currentPicture.height() << std::endl;
-    std::cout << "Label: " << labelWidth << "x" << labelHeight << std::endl;
-
-
-    std::cout << "X1 = " << x1 << std::endl;
-    std::cout << "X2 = " << x2 << std::endl;
-    std::cout << "Y1 = " << y1 << std::endl;
-    std::cout << "Y2 = " << y2 << std::endl;
-
-//    int x1 = pos1.x();
-//    int y1 = pos1.y();
-//    int x2 = pos2.x();
-//    int y2 = pos2.y();
-
     //Gestion des positions
     if (x1 > x2)
     {
@@ -42,14 +28,14 @@ void dessin_model::dessinerRectangle(QPoint pos1, QPoint pos2, int labelWidth, i
         y1 = tmp;
     }
 
-//    if (x1<0)
-//        x1 = -1;
-//    if (y1 < 0)
-//        y1 = -1;
-//    if (x2 > currentPicture.width())
-//        x2 = currentPicture.width();
-//    if (y2 > currentPicture.height())
-//        y2 = currentPicture.height();
+    if (x1<0)
+        x1 = -1;
+    if (y1 < 0)
+        y1 = -1;
+    if (x2 > currentPicture.width())
+        x2 = currentPicture.width();
+    if (y2 > currentPicture.height())
+        y2 = currentPicture.height();
 
     //Dessin du rectangle et affichage sur l'image principale
     currentPicture.draw_rectangle(x1,y1,x2,y2,color,1,~0U);
