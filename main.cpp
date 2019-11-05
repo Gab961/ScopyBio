54bb85b2d07641c1b9950e2088d08bc1461a6780
Prozengan <prozengan@gmail.com>
2019-11-05 14:01:45 +0100

    Stage

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-//    ScopyBio_Controller scopybioController;
-//    scopybioController.startApplication(argc, argv);
     QApplication scopyBio(argc, argv);
 
     MainWindow mainWindow;
