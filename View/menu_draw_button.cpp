a9bb4a55d118f769ab87b099c33909729eaa2831
Erinyth <annalyhse@gmail.com>
2019-11-08 09:31:56 +0100

    Minor changes

--- View/menu_draw_button.cpp
+++ View/menu_draw_button.cpp
@@ -14,17 +14,17 @@ void Menu_Draw_Button::createView()
 
     m_gridTools = new QGridLayout();
 
-    m_selectSquare = new QPushButton("Select square", this);
+    m_selectSquare = new QPushButton("Square", this);
     m_gridTools->addWidget(m_selectSquare, 0, 0);
 
-    m_selectCircle = new QPushButton("Select circle", this);
+    m_selectCircle = new QPushButton("Circle", this);
     m_gridTools->addWidget(m_selectCircle, 1, 0);
 
-    m_selectDraw = new QPushButton("Select draw", this);
-    m_gridTools->addWidget(m_selectDraw, 2, 0);
-
     m_pen = new QPushButton("Pen", this);
-    m_gridTools->addWidget(m_pen, 0, 1);
+    m_gridTools->addWidget(m_pen, 2, 0);
+
+    m_selectDraw = new QPushButton("Select draw", this);
+    m_gridTools->addWidget(m_selectDraw, 0, 1);
 
     m_eraser = new QPushButton("Eraser", this);
     m_gridTools->addWidget(m_eraser, 1, 1);
